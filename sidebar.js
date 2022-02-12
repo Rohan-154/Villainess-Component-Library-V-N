56dbd77b6894dbd56f69f1bbec4ed8ab924ebfd4
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2022-02-12 23:39:38 +0530

    update: added js files

---
+++ sidebar.js
@@ -0,0 +1,5 @@
+const btnToggle = document.querySelector(".land-hamburgerBtn");
+const navbarLinks = document.querySelector(".land-navbar-links");
+btnToggle.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
